7d30be2e3b2a621601209bfc37bc8e959cff39ae
Johann Guilherme Fritzke <135323321+JohannFritzke@users.noreply.github.com>
2025-07-22 00:11:44 -0300

    .

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -7,13 +7,11 @@ import { BaseStats } from "@/components/baseStats/baseStats";
 import { InfoTable } from "@/components/infoTable/infoTable";
 import { Type } from "@/app/api/tipagens";
 import { EvolutionChain } from "@/components/evolutionChain/evolutionChain";
-type PageProps = {
-  params: {
-    name: string;
-  };
+type Props = {
+  params: { name: string };
 };
 
-export default async function Page({params}:PageProps) {
+export default async function Page({params}:Props) {
   const pokemon = await GetPokemon(params.name);
   const number = pokemon.id.toString().padStart(3, "0");
   const type = pokemon.types[0].type.name;
