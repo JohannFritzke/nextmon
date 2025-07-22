648da73f0b28849a5e6d6a14d60b4c0ec8464c25
Johann Guilherme Fritzke <135323321+JohannFritzke@users.noreply.github.com>
2025-07-21 23:55:20 -0300

    ajustes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,13 @@
 import { Search } from "lucide-react";
 import { Input } from "../ui/input";
 import Image from "next/image";
+import Link from "next/link";
 export function Header() {
   return (
     <header className="w-full h-30 flex flex-col justify-center items-center bg-white/5 backdrop-blur-sm shadow-xl gap-5">
       <div className="flex items-center gap-2">
         <Image src="/imgs/icon.svg" alt="NextMon" width={30} height={30} priority />
-        <h1 className="text-xl"><a href="/">NextMon</a></h1>
+        <h1 className="text-xl"><Link href="/">NextMon</Link></h1>
       </div>
       <div className="flex w-100 shadow-lg ">
         <Input
