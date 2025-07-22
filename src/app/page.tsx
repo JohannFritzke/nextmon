a274bad502890bd64974a23f11fe1174ef523dfd
Johann Guilherme Fritzke <135323321+JohannFritzke@users.noreply.github.com>
2025-07-22 00:04:26 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/header/header";
 import { Content } from "@/components/content/content";
 export const dynamic = "force-dynamic";
 
-export default async function Home() {
+export default function Home() {
   return (
     <div className="flex flex-col justify-center">
       <Header />
